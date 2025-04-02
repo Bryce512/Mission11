2c2479139c96c80de2b1f817da5f5870efaf073c
Bryce512 <bryce.whitworth@gmail.com>
2025-04-02 16:56:50 -0600

    switch to hashrouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import AdminBooklist from "./screens/adminBooklist";
 import Cart from "./screens/Cart";
 import Home from "./screens/Home";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
